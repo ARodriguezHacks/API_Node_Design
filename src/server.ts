c4c2a6e297c0aee0309cb8b12a0537161d7b03ed
Angie Rodriguez <angrodri902@gmail.com>
2023-09-08 20:10:09 -0700

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import router from './router'
+import morgan from "morgan"
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use((req, res, next) => {
+  req.secret = "kitty"
+  res.status(401)
+  res.send('Nope')
+})
+
+app.get("/", (req, res) => {
+  console.log("hello from express");
+  res.status(200);
+  res.json({ message: "hello" });
+});
+
+app.use('/api', router)
+
+export default app;
